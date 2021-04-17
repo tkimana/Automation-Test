9886bf853c2b92ef037a9e247e4fa1fabe692ac9
Theodore <kimana.theodore>
2021-04-17 13:44:37 -0500

    Automation project

---
+++ Helpers/Homepage.js
@@ -0,0 +1,35 @@
+const {Builder, By, Key, util} = require("selenium-webdriver");
+
+
+function navigateToHomePage(driver){
+    driver.get("http://demo.guru99.com/test/newtours/index.php")
+
+}
+
+function navigateToRegisterPage(driver){
+    driver.findElement(By.linkText("REGISTER")).click();
+
+}
+
+function navigateToSignon(driver){
+    driver.findElement(By.linkText("SIGN-ON")).click();
+
+}
+
+function navigateToFlights(driver){
+    driver.findElement(By.linkText("Flights")).click();
+
+}
+
+
+module.exports = {
+    navigateToHomePage,
+    navigateToRegisterPage,
+    navigateToSignon,
+    navigateToFlights
+}
+
+
+   
+
+
---
+++ Tests/Test.js
@@ -0,0 +1,21 @@
+const homePage = require('../Helpers/Homepage.js')
+const registerPage = require('../Helpers/Registerpage.js')
+var webDriver = require("selenium-webdriver")
+const assert = require("assert")
+var driver = new webDriver.Builder().forBrowser('chrome').build();
+
+
+async function testOne(){
+   homePage.navigateToHomePage(driver)
+   homePage.navigateToRegisterPage(driver)
+   await driver.sleep(8000);
+   registerPage.fillForm(driver)
+   await driver.sleep(9000);
+   driver.close();
+}
+
+
+
+testOne();
+
+    
